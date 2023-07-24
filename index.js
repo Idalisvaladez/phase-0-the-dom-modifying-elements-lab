a88d4fc570da9c42116335c7e00ad0aa685218ff
Idalis Valadez <idalisvaladez22@gmail.com>
2023-07-24 16:38:07 -0500

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
 // Write your code here!
+document.getElementById("main").remove();
+
+
+const newHeader = document.createElement("h1");
+newHeader.id = 'victory'
+newHeader.innerHTML = "Idalis is the champion"
+document.h1.append(newHeader);
+
+
+
+
+/*
+const id = document.createElement("id");
+id.style.value = "victory";
+newHeader.append(id);
+
+
+
+ document.newHeader.setAttribute("id", "victory");
+
+
+ const id = document.createElement("id");
+document.h1.append(id);
+
+document.getElementById("id").innerHTML = "victory";
+
+
+
+/* const id = document.createElement("id");
+id.className = "victory";
+
+newHeader.append(id); 
+
+
+
+newHeader.textContent = "Idalis is the champion"; */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
